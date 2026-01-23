79fdb04b5047883ff65183cfb0f3661d68c9904d
MartinPerezDev <martinperez9713@gmail.com>
2026-01-23 10:51:41 -0300

    2 commit

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 const admin = process.env.ADMIN
-const fileProducts = new FileManager("./src/data/products.json")
+const fileProducts = new FileManager("./src/data/products.js")
 
 export const get = async (req, res)=>{
     try{
