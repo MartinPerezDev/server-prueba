71f558dc81117ba530321ab8dc8490e2fc5403db
MartinPerezDev <martinperez9713@gmail.com>
2026-03-17 19:24:46 -0300

    fix data products

--- src/data/products.js
+++ src/data/products.js
@@ -1,84 +1,29 @@
 [
   {
     "id": 1,
-    "name": "Auriculares Ares H120",
-    "description": "Con su bajo peso y sus generosas almohadillas en las copas y en la vincha, los Ares realmente minimizan el estrés del contacto y resultan muy cómodos, incluso en el uso prolongado.",
+    "name": "Mouse G502",
+    "description": "Diseñado para un desempeño avanzado en los juegos. G502 HERO cuenta con un sensor de juegos HERO 25K con seguimiento de precisión submicrométrica, LIGHTSYNC RGB personalizable, perfiles integrados, pesos reposicionables y mucho más.",
     "stock": 8,
-    "image": "/image/ARES.png",
+    "image": "/image/g502.webp",
     "price": 450,
-    "category": "auriculares"
+    "category": "mouse"
   },
   {
     "id": 2,
-    "name": "Auriculares Lamia 2 H320",
-    "description": "Todos los componentes de los Lamia2 están diseñados para durar. La diadema metálica y el brazo flexible del micrófono admiten una enorme amplitud de tensión, el cable resiste miles de flexiones, y la firme armazón de las copas, a prueba de golpes, protege fiablemente los componentes internos. ¡Son muy sólidos!",
+    "name": "Auriculares G325",
+    "description": "Sumérgete en el audio del juego. Estos auriculares inalámbricos para gaming ofrecen audio dinámico de 24 bits, un micrófono con formación de haces integrado y más de 24 horas de autonomía. Disfruta de una comodidad excepcional gracias a su diseño ajustable y estilizado.",
     "stock": 7,
-    "image": "/image/LAMIA.png",
-    "price": 4000,
-    "category": "auriculares",
-    "timestamp": "23/01/2026 12:59"
-  },
-  {
-    "id": 3,
-    "name": "Auriculares Redragon Cybill H312",
-    "description": "Robustos, cómodos y con una calidad de sonido impactante; los Cybill son audífonos USB diseñados para que puedas disfrutar de una experiencia 360° en tus juegos favoritos.",
-    "stock": 8,
-    "image": "/image/Cybill.png",
-    "price": 450,
+    "image": "/image/g325.webp",
+    "price": 550,
     "category": "auriculares"
   },
   {
-    "id": 4,
-    "name": "Mouse King M724",
-    "description": "El K1ng es un ratón híper competitivo que esquiva los ornamentos innecesarios para concentrarse plenamente en el rendimiento.",
-    "stock": 6,
-    "image": "/image/KING-M724-B.png",
-    "price": 300,
-    "category": "mouse"
-  },
-  {
-    "id": 5,
-    "name": "Mouse M601RGB",
-    "description": "Por su muy buen diseño, por su excelente rendimiento, por su prolongada vida útil, el Centrophorus es y seguirá siendo uno de los grandes preferidos del público. Sencillamente, sabes que puedes confiar en él.",
-    "stock": 16,
-    "image": "/image/CETROPHORUS-RGB.png",
-    "price": 200,
-    "category": "mouse"
-  },
-  {
-    "id": 6,
-    "name": "Mouse King Pro M916",
-    "description": "100% enfocado en el rendimiento, el K1ng Pro es nuestro ratón tope de gama para los jugadores más competitivos.",
-    "stock": 8,
-    "image": "/image/KING-PRO-1K-BLACK-M916.png",
-    "price": 450,
-    "category": "mouse"
-  },
-  {
-    "id": 7,
-    "name": "Webcam Hitman GW800",
-    "description": "Transmite, enseña, entretiene; muéstrate haciendo lo que mejor haces. La Hitman es la herramienta perfecta para ello: una webcam diseñada específicamente para streaming, que se adapta a las necesidades precisas de los nuevos y futuros creadores de contenido del gaming y la tecnología",
-    "stock": 2,
-    "image": "/image/HITMAN-800.png",
-    "price": 310,
-    "category": "streaming"
-  },
-  {
-    "id": 8,
-    "name": "Microfono Blazar GM300",
-    "description": "El Blazar es un micrófono de condensador optimizado para streaming, con características idóneas para la captura y la grabación o la transmisión en vivo y un diseño cómodo, altamente funcional, que luce muy bien en cámara. Elevará perceptiblemente la calidad de tus transmisiones.",
-    "stock": 8,
-    "image": "/image/BLAZAR.png",
-    "price": 450,
-    "category": "streaming"
-  },
-  {
-    "id": 9,
-    "name": "Microfono Quasar GM200",
-    "description": "El Quasar constituye el salto de calidad sonoro que necesitas para afrontar el streaming con profesionalismo. Un micrófono de condensador que captura la voz con nitidez excelente sin requerir de tu parte conocimientos técnicos sofisticados en ingeniería de sonido. Simple, práctico, efectivo.",
-    "stock": 5,
-    "image": "/image/QUAZAR-1.png",
-    "price": 540,
-    "category": "streaming"
+    "id": 3,
+    "name": "Teclado G213",
+    "description": "Durabilidad apta para los requisitos de los videojuegos. Los interruptores de tecla Mech-Dome táctiles son resistentes a salpicaduras. Iluminación RGB LIGHTSYNC personalizable. Reposamanos integrado y patas ajustables. Controles multimedia específicos.",
+    "stock": 4,
+    "image": "/image/g213.png",
+    "price": 700,
+    "category": "teclados"
   }
 ]
